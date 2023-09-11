8fd18cca63b059627573253dacb15e8c05de1784
I461-TKI <lukankinv20@gmail.com>
2023-09-11 22:37:11 +0300

    Update task1-3.c
    Переделанный 1-3 номер

--- task1-3.c
+++ task1-3.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
+/**
+ * @brief Функция рассчитывает потенциальную энергию
+ * @param k, x; где k - жесткость пружины, x - растянутость пружины
+*/
+double Potential_energy(double k, double x);
+/**
+ * @brief Функция проверяет, что в переменной (в функции массиве) присутствуют только цифры или вспомогательные знаки ('.')
+ * @param *check_param является указателем (адресом) для проверяемой переменной
+ * @return 0 => В переменной есть другие символы, 1 => В переменной только цифры
+*/
+int is_num(double *check_param);
 
 int main(){
-    float k, x, res;
-    printf("Insert the k value first, then the x value, all values is number\n");
-    scanf("%f", &k);
-    scanf("%f", &x);
-    if (k >= 0.0 && x >= 0.0){
-        res = (k*x*x)/2;
-        printf("Potential energy equals: %f\n", res);
+    double k, x, energy;
+    printf("Insert the k value first, then the x value, all values is non-negative number\n");
+
+    while(!is_num(&k)){
+        printf("Insert a non-negative number\n");
     }
-    else {
-        printf("Insert non-negative numbers\n");
+
+    while(!is_num(&x)){
+        printf("Insert a non-negative number\n");
     }
+
+    energy = Potential_energy(k, x);
+    printf("Potential energy equals: %.2f", energy);
+
+    return 0;
+}
+
+double Potential_energy(double k, double x){
+    double Poten_energy;
+    Poten_energy = (k*x*x)/2;
+    return Poten_energy;
+}
+
+int is_num(double *check_param) {
+    char buffer[16];
+    memset(&buffer, '\0', 16);
+    scanf("%s", &buffer);
+    for(int i = 0; i < 16; i++){
+        if((buffer[i] < 48 || buffer[i] > 57) && buffer[i] != '.' && buffer[i] != '\0'){
+            return 0;
+        }
+    }
+    sscanf(buffer, "%lf", check_param);
+    return 1;
 }
