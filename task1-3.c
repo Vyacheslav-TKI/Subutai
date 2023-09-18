5bfa3231a4af44c94213e1ae605ba2cb6a16b20d
I461-TKI <lukankinv20@gmail.com>
2023-09-18 13:46:05 +0300

    Update task1-3.c
    Update 18.09 1-3

--- task1-3.c
+++ task1-3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 /**
  * @brief Функция рассчитывает потенциальную энергию
@@ -9,45 +8,38 @@
 */
 double Potential_energy(double k, double x);
 /**
- * @brief Функция проверяет, что в переменной (в функции массиве) присутствуют только цифры или вспомогательные знаки ('.')
- * @param *check_param является указателем (адресом) для проверяемой переменной
- * @return 0 => В переменной есть другие символы, 1 => В переменной только цифры
+ * @brief Функция проверяет на положительность или нейтральность(число==0) числа
+ * @param check_param проверяемая переменная
+ * @return 0 => Число отрицательное, 1 => Число положительное или равно нулю
 */
-int is_num(double *check_param);
+int is_num(double check_param);
 
 int main(){
     double k, x, energy;
-    printf("Insert the k value first, then the x value, all values is non-negative number\n");
+    printf("%s", "Insert the k value first, then the x value, all values is non-negative number\n");
 
-    while(!is_num(&k)){
-        printf("Insert a non-negative number\n");
+    scanf("%lf", &k);
+    scanf("%lf", &x);
+
+    if(is_num(k) && is_num(x)){
+
+        energy = Potential_energy(k, x);
+        printf("Potential energy equals: %.2f", energy);
+
+    
     }
-
-    while(!is_num(&x)){
-        printf("Insert a non-negative number\n");
-    }
-
-    energy = Potential_energy(k, x);
-    printf("Potential energy equals: %.2f", energy);
-
     return 0;
 }
 
 double Potential_energy(double k, double x){
     double Poten_energy;
-    Poten_energy = (k*x*x)/2;
+    Poten_energy = (k * x * x) / 2;
     return Poten_energy;
 }
 
-int is_num(double *check_param) {
-    char buffer[16];
-    memset(&buffer, '\0', 16);
-    scanf("%s", &buffer);
-    for(int i = 0; i < 16; i++){
-        if((buffer[i] < 48 || buffer[i] > 57) && buffer[i] != '.' && buffer[i] != '\0'){
-            return 0;
-        }
+int is_num(double check_param) {
+    if (check_param >= 0.0f){
+        return 1;
     }
-    sscanf(buffer, "%lf", check_param);
-    return 1;
+    return 0;
 }
