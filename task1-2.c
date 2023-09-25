a6a690cc077fb570449ea630b07d323daaeac39a
I461-TKI <lukankinv20@gmail.com>
2023-09-25 18:47:53 +0300

    Update task1-2.c
    1-2 3-я итерация

--- task1-2.c
+++ task1-2.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
 
@@ -7,12 +6,12 @@
  * @brief Функция считает среднее арифметическое кубов заданных чисел
  * @param x, y являются рациональными числами
  */
-double Srednee_arifmetich_cubov(double x, double y);
+double Arithmetic_mean_of_cubes(double x, double y);
 /**
  * @brief Функция считает среднее геометрическое МОДУЛЕЙ чисел
  * @param x, y являются рациональными числами
 */
-double Srednee_geometricheskoye(double x, double y);
+double geometric_mean(double x, double y);
 
 int main(){
     double i, j, sr_arifm_cube, sr_geom;
@@ -22,22 +21,22 @@ int main(){
     scanf("%lf", &i);
     scanf("%lf", &j);
 
-    sr_arifm_cube = Srednee_arifmetich_cubov(i, j);
-    sr_geom = Srednee_geometricheskoye(i, j);
+    sr_arifm_cube = Arithmetic_mean_of_cubes(i, j);
+    sr_geom = geometric_mean(i, j);
     printf("The arithmetic mean of cubes of numbers is equal to: %.2f\n", sr_arifm_cube);
     printf("The geometric mean of the numbers is: %.2f\n", sr_geom);
     
     return 0;
 
 }
-double Srednee_arifmetich_cubov(double x, double y){
+double Arithmetic_mean_of_cubes(double x, double y){
     double cube_x, cube_y;
     cube_x = x * x * x;
     cube_y = y * y * y;
     return (cube_x + cube_y) / 2;
 }
 
-double Srednee_geometricheskoye(double x, double y){
+double geometric_mean(double x, double y){
     x = abs(x);
     y = abs(y);
     return sqrt(x * y);
