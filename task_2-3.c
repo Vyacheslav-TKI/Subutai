78cdba636246e3db508cca38bbdf5b29d4a92669
I461-TKI <lukankinv20@gmail.com>
2023-09-29 21:17:06 +0300

    Update task_2-3.c
    2-3 перепиленный

--- task_2-3.c
+++ task_2-3.c
@@ -7,28 +7,27 @@
  * @param x проверяемое число
  * @return 1 - число отрицательное или равное нулю, 0 - число положительное
 */
-int is_poz_num(double x);
+int is_pozitive_num(double x);
 
 int main(){
     float first_side, second_side, third_side;
     
-    printf("%s", "In the next(3) strings enter non-negative numbers\n");
+    puts("In the next(3) strings enter non-negative numbers\n");
     scanf("%f", &first_side);
     scanf("%f", &second_side);
     scanf("%f", &third_side);
-    system("pause");
 
-    if (is_poz_num(first_side) || is_poz_num(second_side) || is_poz_num(third_side)){
-        printf("%s", "Please, enter non-negative number\n");
+    if (is_pozitive_num(first_side) || is_pozitive_num(second_side) || is_pozitive_num(third_side)){
+        puts("Please, enter non-negative number\n");
     }
     else{
         if((first_side+second_side > third_side) && (first_side + third_side > second_side) && (second_side+third_side > first_side)){
-            printf("%s", "It is possible to make a triangle from these sides\n");
+            puts("It is possible to make a triangle from these sides\n");
             if((first_side*first_side < second_side*second_side+third_side*third_side) && (second_side*second_side < first_side*first_side+third_side*third_side) && (third_side*third_side < first_side*first_side+second_side*second_side)){
-                printf("%s", "Acute triangle\n");
+                puts("Acute triangle\n");
             }
             else {
-                printf("%s", "Non-acute triangle\n");
+                puts("Non-acute triangle\n");
             }
         }
     }
@@ -36,7 +35,7 @@ int main(){
 
 }
 
-int is_poz_num(double x){
+int is_pozitive_num(double x){
     if(x > 0){
         return 0;
     }
