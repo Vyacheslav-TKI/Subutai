f6873244d7c4318df3027fc0075c10a25f95271a
I461-TKI <lukankinv20@gmail.com>
2023-09-29 20:36:22 +0300

    Update task1-2.c
    1-2 допилка

--- task1-2.c
+++ task1-2.c
@@ -6,7 +6,7 @@
  * @brief Функция считает среднее арифметическое кубов заданных чисел
  * @param x, y являются рациональными числами
  */
-double Arithmetic_mean_of_cubes(double x, double y);
+double arithmetic_mean_of_cubes(double x, double y);
 /**
  * @brief Функция считает среднее геометрическое МОДУЛЕЙ чисел
  * @param x, y являются рациональными числами
@@ -16,12 +16,12 @@ double geometric_mean(double x, double y);
 int main(){
     double i, j, sr_arifm_cube, sr_geom;
 
-    printf("%s", "In the next strings insert just non-negative or just negative numbers\n");
+    puts("In the next strings insert just non-negative or just negative numbers\n");
 
     scanf("%lf", &i);
     scanf("%lf", &j);
 
-    sr_arifm_cube = Arithmetic_mean_of_cubes(i, j);
+    sr_arifm_cube = arithmetic_mean_of_cubes(i, j);
     sr_geom = geometric_mean(i, j);
     printf("The arithmetic mean of cubes of numbers is equal to: %.2f\n", sr_arifm_cube);
     printf("The geometric mean of the numbers is: %.2f\n", sr_geom);
@@ -29,7 +29,7 @@ int main(){
     return 0;
 
 }
-double Arithmetic_mean_of_cubes(double x, double y){
+double arithmetic_mean_of_cubes(double x, double y){
     double cube_x, cube_y;
     cube_x = x * x * x;
     cube_y = y * y * y;
