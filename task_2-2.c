1d4e4f01de5ab56cf46fb0bb3ceae95c12ab8113
I461-TKI <lukankinv20@gmail.com>
2023-09-29 21:16:57 +0300

    Update task_2-2.c
    2-2 перепиленный

--- task_2-2.c
+++ task_2-2.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * @brief Функция вычисляет первое(верхнее уравнение)
+ * @param a Заданная константа
+ * @param x Заданная пользователем переменная
+*/
+double first_equation(double a, double x);
+
+/**
+ * @brief Функция вычисляет второе(нижнее) уравнение
+ * @param a Заданная константа
+ * @param x Заданная пользователем переменная
+*/
+double second_equation(double a, double x);
+
 int main(){
-    float a = 0.3f;
-    float x;
-    float y;
-    printf("Enter value of x\n");
+    double a = 0.3f;
+    double x;
+    double y;
+    puts("Enter value of x\n");
     scanf("%f", &x);
 
     if(a*x < 1){
-        y = a * sin((x*x+1)/10);
-        printf("The value of y equals: %f", y);
+        first_equation(a, x);
     }
     else if(a*x >= 1){
-        y = a * cos((x-1)/10);
-        printf("The value of y equals: %f", y);
+        second_equation(a, x);
     }
     return 0;
+}
+
+double first_equation(double a, double x){
+    double y = a * sin((x*x+1)/10);
+    printf("The value of y equals: %f", y);
+}
+
+double second_equation(double a, double x){
+    double y = a * cos((x-1)/10);
+    printf("The value of y equals: %f", y);
 }
