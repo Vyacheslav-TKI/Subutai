fbe0206fd25e0e3e8810fde80fe254a9ac7a6744
I461-TKI <lukankinv20@gmail.com>
2023-09-29 20:36:29 +0300

    Update task1-3.c
    1-3 допилка

--- task1-3.c
+++ task1-3.c
@@ -6,7 +6,7 @@
  * @param k - жесткость пружины,
  * @param x - растянутость пружины
 */
-double Potential_energy(double k, double x);
+double potential_energy(double k, double x);
 /**
  * @brief Функция проверяет на положительность или нейтральность(число==0) числа
  * @param check_param проверяемая переменная
@@ -16,28 +16,28 @@ int is_num(double check_param);
 
 int main(){
     double k, x, energy;
-    printf("%s", "Insert the k value first, then the x value, all values is non-negative number\n");
+    puts("Insert the k value first, then the x value, all values is non-negative number\n");
 
     scanf("%lf", &k);
     scanf("%lf", &x);
 
-    if(is_num(k) && is_num(x)){
+    if(is_num(k) + is_num(x) < 2){
+        printf("%s", "Insert only pozitive numbers!\n");
+        return 0;
+        abort();
 
-        energy = Potential_energy(k, x);
-        printf("Potential energy equals: %.2f", energy);
-
-    
     }
     else {
-        printf("%s", "Insert only pozitive numbers!\n");
+        energy = potential_energy(k, x);
+        printf("Potential energy equals: %.2f", energy);
     }
     return 0;
 }
 
-double Potential_energy(double k, double x){
-    double Poten_energy;
-    Poten_energy = (k * x * x) / 2;
-    return Poten_energy;
+double potential_energy(double k, double x){
+    double poten_energy;
+    poten_energy = (k * x * x) / 2;
+    return poten_energy;
 }
 
 int is_num(double check_param) {
