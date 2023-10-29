9856ad8b6e88da99502e03ccca2b49e6bdc439dd
I461-TKI <lukankinv20@gmail.com>
2023-10-29 21:57:24 +0300

    Update task_2-1.c
    Нюанс с 2-1

--- task_2-1.c
+++ task_2-1.c
@@ -9,24 +9,31 @@
 *@return 1 - число положительное и отличное от нуля, 0 - число отрицательное или нуль
 */
 int is_positive(const double x);
+
 /**
 *@brief Функция вычисляет нужную пользователю искомую величину
 *@param x порядковый номер формулы
 */
 double formula_selection(int x, double cube_edge);
+
 /**
 *@brief Функция сверяет введенное значение из переменной formula
 *@param x значение переменной formula
 *@return 1 - введенное число принадлежит диапазону [1;3]
 */
 int is_right_number(const int x);
+
 enum Values
 {
-    face_area = 1,
-    total_surface = 25,
-    volume = 3,
+face_area = 1,
+total_surface = 2,
+volume = 3,
 };
-
+/**
+*@brief Точка входа в программу
+*@return 0 программа работает правильно
+*@return 1 в программе есть ошибка
+*/
 int main(){
 
     double cube_edge;
@@ -35,25 +42,23 @@ int main(){
 
     if(scanf("%lf", &cube_edge) != 1){
         puts("Please insert a valid value\n");
-        abort();}
+        return 1;}
     if (is_positive(cube_edge) == 0){
         puts("Please, enter a non-negative number!\n");
-        abort();
-    }
+        return 1;
+        }
 
     puts("Please, write what you want to find: face area, total surface area or volume\n");
     printf("If you want to find face area, press %d\n", (enum Values)(face_area));
     printf("If you want to find total surface, press %d\n", (enum Values)(total_surface));
     printf("If you want to find volume, press %d\n", (enum Values)(volume));
 
-
-
     if(scanf("%d", &formula) != 1){
         puts("Please input a valid value!\n");
-        abort();}
+        return 1;}
     if(is_right_number(formula) == 0){
-        puts("Please, enter 1, 2 or 3!\n");
-        abort();
+        printf("Please, enter %d, %d or %d!\n", (enum Values)(face_area), (enum Values)(total_surface), (enum Values)(volume));
+        return 1;
     }
     else{
         formula_selection(formula, cube_edge);
@@ -62,30 +67,23 @@ int main(){
 }
 
 int is_right_number(const int x){
-    if(x > 0 && x < 4){
-        return 1;
-    }
-    return 0;
+    return (x > 0 && x < 4) ? 1 : 0;
 }
 
-int is_positive(const double x){
-    if(x>0){
-        return 1;
-    }
-    return 0;
+int is_positive(const double x){ // Зачем мне тут использовать bool?
+    return (x > 0) ? 1 : 0;
 }
 
-double formula_selection(int x, double cube_edge){ // раздробить?
-    enum Values face = face_area, total = total_surface, volume_cube = volume;
-    if(x == face){
+double formula_selection(int x, double cube_edge){
+    if(x == (enum Values)(face_area)){
         double x = cube_edge*cube_edge;
         printf("Face area equals: %lf", x);
     }
-    else if(x == total){
+    else if(x == (enum Values)(total_surface)){
         double y = 6 * cube_edge * cube_edge;
         printf("Total surface equals: %lf", y);
     }
-    else if(x == volume_cube){
+    else if(x == (enum Values)(volume)){
         double z = cube_edge*cube_edge*cube_edge;
         printf("Volume equals: %lf", z);
     }
