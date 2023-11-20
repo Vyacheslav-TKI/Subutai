b9b0d24f94afcf4bc3259d9066d74840684b80a5
Vyacheslav-TKI <61204893+Vyacheslav-TKI@users.noreply.github.com>
2023-11-20 10:40:53 +0300

    Update 3-1.c
    +функция

--- 3-1.c
+++ 3-1.c
@@ -19,6 +19,14 @@ double check_number();
 bool check_function(double x);
 
 /**
+ * @brief Проверяет начальные и конечные значения на то, чтобы начальное значение меньше конечного
+ * @param x_start начальное значение
+ * @param x_stop конечное значение
+ * @return true начальное значение меньше конечного
+ */
+bool check_arguments(double x_start, double x_stop);
+
+/**
  * @brief Точка входа в программу
  * @return 0 Программа исправна
 */
@@ -29,6 +37,8 @@ int main(){
     double x_stop = check_number();
     puts("Insert a step number\n");
     double x_step = check_number();
+
+    check_arguments(x_start, x_stop);
     
     while (x_start < x_stop + x_step)
     {
@@ -60,6 +70,15 @@ bool check_function(double x){
     if ((x + cos(powf(x, 0.52)+2) + DBL_EPSILON) < -0.57 + DBL_EPSILON || x < DBL_EPSILON)
     {
         return false;
+    }
+    return true;
+}
+
+bool check_arguments(double x_start, double x_stop){
+    if (x_start > x_stop)
+    {
+        puts("Start value is bigger than stop value!\n);
+        abort();
     }
     return true;
 }
